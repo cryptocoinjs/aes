879ba3727f09aaf3fa5a94b391d4bf9f71dd0890
JP Richardson <jprichardson@gmail.com>
2014-02-22 08:37:30 -0600

    removed bitArray

--- test/aes_test.js
+++ test/aes_test.js
@@ -1,8 +1,44 @@
 var testVectors = require('./aes_vectors').test_vectors;
 var AES = require('../lib/aes');
-var bitArray = require('../lib/bitArray');
+var util = require('util')
 
 require('terst')
+
+function ARR_EQ (a1, a2, msg) {
+  if (a1.length != a2.length)
+    throw new Error("a1.length != a2.length.\n\n" + msg);
+  //console.dir(a1)
+  //console.dir(a2)
+  for (var i = 0; i < a1.length; ++i) {
+    if (a1[i] !== a2[i]) {
+      var errMsg = "a1[%d]: %d !== a2[%d]: %d.\n\n" + msg;
+      throw new Error(util.format(i, a1[i], i, a2[i]));
+    }
+  } 
+}
+
+/*function convertEndian(num32) {
+  var ret = 0x0;
+  return ret = (num32 >>> 24) |
+        ((num32 >>> 8) & 0x0000FF00) |
+        ((num32 << 8) & 0x00FF0000) |
+        ((num32 << 24)) 
+}
+
+function convArray(arr) {
+  for (var i = 0; i < arr.length; ++i) {
+    arr[i] = convertEndian(arr[i])
+  }
+  return arr
+}
+
+var tvs = [];
+for (var i = 0; i < testVectors.length; ++i) {
+  var obj = testVectors[i]
+  //obj.key = new Uint32Array(convArray(obj.key))
+  //obj.pt = new Uint32Array(convArray(obj.pt))
+  //obj.ct = new Uint32Array(convArray(obj.ct))
+}*/
 
 describe('AES official known-answer tests', function() {
   it('they should all pass', function() {
@@ -12,8 +48,8 @@ describe('AES official known-answer tests', function() {
       var tv = kat[i];
       var len = 32 * tv.key.length;
       var aes = new AES(tv.key);
-      T (bitArray.equal(aes.encrypt(tv.pt), tv.ct), "encrypt "+len+" #"+i);
-      T (bitArray.equal(aes.decrypt(tv.ct), tv.pt), "decrypt "+len+" #"+i);
+      ARR_EQ(aes.encrypt(tv.pt), tv.ct, "encrypt "+len+" #"+i);
+      ARR_EQ(aes.decrypt(tv.ct), tv.pt, "decrypt "+len+" #"+i);
     }
   })
 })
